8bfadaca53709d18fdd5397379e9d1d2a9383538
ahaic <ahaic@users.noreply.github.com>
2019-08-24 20:46:51 +0800

    Update esp8266.py

--- esp8266.py
+++ esp8266.py
@@ -16,10 +16,11 @@ print(wlan)
 
 # for esp8266  25 aug 2019 
 
-#esp8266  d8 = pin 15  
+#esp8266  d8 = pin 15    d6 =12
 
-import dht, machine,time
-d = dht.DHT11(machine.Pin(15))
+import dht, machine,time,utime
+
+d = dht.DHT11(machine.Pin(12))
 
 
 def temp_hum():
@@ -30,7 +31,7 @@ def temp_hum():
   
   return temp, hum
 
-#print(temp_hum())
+print(temp_hum())
 
 
 
@@ -39,22 +40,49 @@ def temp_hum():
 def barmeter():
  
   from bmp280 import BMP280
-  bus = machine.I2C(sda=machine.Pin(21),scl=machine.Pin(22))
-  bmp = BMP280(bus)
+  try:
+    bus = machine.I2C(sda=machine.Pin(4),scl=machine.Pin(5))
+    bmp = BMP280(bus)
    
-  print(bmp.temperature)
-  print(bmp.pressure)
+    print(bmp.temperature)
+    print(bmp.pressure)
+  except:
+    print('bar meter error reading ')
   
   
 def led():
 
   p2 = machine.Pin(2, machine.Pin.OUT)
-  p2.value(1)
-  time.sleep(1)
-  p2.value(0)
+  for i in range(100):
+    p2.value(1)
+    time.sleep(1)
+    p2.value(0)
+    
+    
+    
+    
+    
+def pwm_led():   
+
+  p2 = machine.Pin(2)
+  pwm2 = machine.PWM(p2)
+  pwm2.freq(500)
+  while True:
+      for duty in range(0,1023,80):
+          pwm2.duty(duty)
+          utime.sleep_ms(150)
+      for duty in range(1023, 0,-80):
+          pwm2.duty(duty)
+          utime.sleep_ms(150)
+          
+        
   
-for i in range(100):
-  led()
   
+        
+pwm_led()
+  
+barmeter()
+
+
   
  
