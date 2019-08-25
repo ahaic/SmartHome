255f34612978386c8b2ff10f06783f8214377ec1
ahaic <ahaic@users.noreply.github.com>
2019-08-25 21:35:25 +0800

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,19 +1,24 @@
-#esp8266
 
-def do_connect():
+
+
+def wifi_connect():
     import network
-
-    SSID = 'SSID'
-    PASSWORD = 'PASSWORD'
-
-    sta_if = network.WLAN(network.STA_IF)
-    ap_if = network.WLAN(network.AP_IF)
-    if ap_if.active():
-        ap_if.active(False)
-    if not sta_if.isconnected():
+    wifi = network.WLAN(network.STA_IF)
+    wifi.active(True)
+    if not wifi.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(SSID, PASSWORD)
-        while not sta_if.isconnected():
+        wifi.connect('XXX', 'XXXX')
+        while not wifi.isconnected():
             pass
-    print('Network configuration:', sta_if.ifconfig())
+    print('network config:', wifi.ifconfig())
+
+
+
+wifi_connect()
+
+
+import publiser
+
+
+
+print('-----initialied ----')
